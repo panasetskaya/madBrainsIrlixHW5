30141a2ebd4dba824b0d88cf11158ae724460043
Maria Panasetskaia <mariapolikutina@gmail.com>
2022-03-15 16:49:54 +0800

    added: Settings (Linear layout)

--- app/src/main/java/com/example/madbrainsirlixhomew5/MainActivity.kt
+++ app/src/main/java/com/example/madbrainsirlixhomew5/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.madbrainsirlixhomew5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var menuButton: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val actionBar = supportActionBar
         if (actionBar!=null) actionBar.hide()
+        menuButton = findViewById(R.id.imageViewMenuIcon)
+        menuButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/madbrainsirlixhomew5/SettingsActivity.kt
+++ app/src/main/java/com/example/madbrainsirlixhomew5/SettingsActivity.kt
@@ -7,5 +7,7 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        val actionBar = supportActionBar
+        if (actionBar!=null) actionBar.hide()
     }
 }
